a13fc1076f421bc266fc55e65fe77d9096846c93
Asfar Sharief <asfar.sharief@eagleview.com>
2022-04-19 20:11:09 +0530

    Added logic to match same url sent in different format

--- lib/urlshortner.go
+++ lib/urlshortner.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/asfarsharief/URL-Shortener/constants"
 	"github.com/asfarsharief/URL-Shortener/storage"
@@ -24,12 +25,18 @@ func NewUrlShortnerService(storageHandler storage.StorageHandlerInterface) urlSh
 
 func (us *urlShortnerService) GetShortUrl(url string) (string, error) {
 	fmt.Println("Given URL : ", url)
-
 	if ok, _ := regexp.MatchString(constants.UrlValidatorRegex, url); !ok {
 		str := fmt.Sprintf("Invalid URL. Please validate. - %s", url)
 		fmt.Println(str)
 		return "", errors.New(str)
 	}
 
+	if strings.Contains(url, "http://") || strings.Contains(url, "https://") {
+		url = strings.Split(url, "//")[1]
+	}
+	urlArr := strings.Split(url, ".")
+	if urlArr[0] == "www" {
+		url = url[4:]
+	}
 	return us.storageHandler.ProcessUrl(url)
 }
